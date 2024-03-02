818a07c670bdb2a1a29ee1c8ead2c2d1ae7f078a
ANNAS HAMEED QAZI <154601533+ANNASHAMEEDQAZI@users.noreply.github.com>
2024-03-02 21:54:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,68 @@
+// VARIABLES FOR STRING
+// Q1: Declare a variable called username.
+// var username;
+// alert(username);
+
+// Q2: Declare a variable called myName & assign to it a string that represents your Full Name.
+// var myName = "Annas Hameed Qazi"
+// alert(myName);
+
+// Q3: Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+// var JS= "Hello World";
+// alert(JS);
+
+// Q4: Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+// var name = "Annas Hameed Qazi"
+// var age = "24 years old";
+// alert(name);
+// alert(age);
+
+// Q5: Write a script to display the following alert using one JS variable:
+var pizza = "PIZZA \nPIZZ \nPIZ \nPI \nP";
+alert(pizza);
+// Q6: Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com). Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+// var email = "My email address is annashameedqazi@gmail.com"
+// alert(email);
+
+// Q7: Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box:
+// var book = "A smarter way to learn JavaScript."
+// alert(book);
+
+// Q8: Write a script to display this in browser through JS.
+// document.write("Yeah! I can write HTML content through JavaScript")
+
+// Q9: Store following string in a variable and show in alert and browser through JS.
+// var myStyle = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// document.write(myStyle);
+// alert(myStyle);
+
+// VARIABLES FOR NUMBER
+
+// Q1: Declare a variable called age & assign to it your age. Show your age in an alert box.
+// var age = 24;
+// alert("I'm " +age+ " years old");
+
+// Q2: Declare & initialize a variable to keep track of how many times a visitor has visited a web page. Show his/her number of visits on your web page. For example: “You have visited this site N times”.
+// var visited = 14;
+// alert("You've visited this site " +visited+ " times");
+// Q3: Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+// var birthYear = 1999;
+// document.write("My birth year is ", birthYear, "<br\>");
+// document.write("Data type of my declared variable is ", typeof(birthYear));
+
+// Q4: A visitor visits an online clothing store www.xyzClothing.com . Write a script to store in variables the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+// Show the following message in your browser: “John
+// Do ordered 5 T-shirt(s) on XYZ Clothing store”.
+// var customerName = "AHQ";
+// var product = "t-shirts";
+// var quantity = 5;
+// document.write(customerName, " ordered ", quantity, " ", product, " on XYZ CLOTHING")
+
+
